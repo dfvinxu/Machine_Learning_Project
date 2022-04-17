00fa8070a6c791b47deb0cdd656bc9ff936091ff
dfvinxu <df.vinxu@gmail.com>
2022-04-17 21:00:20 +0200

    Ultimos cambios

--- src/train.py
+++ src/train.py
@@ -1,8 +1,5 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.impute import KNNImputer
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.ensemble import BalancedBaggingClassifier
 
